e04cdac3e88babe08bcb834818f3cfa3f6dc6158
Martin Guevara <martin_91@live.com.ar>
2022-07-22 13:09:52 -0300

    Se agrego Material UI al proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import NavBar from './components/NavBar';
 
+import * as React from 'react';
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div>
-      <NavBar/>
-        <h1></h1>
+      <NavBar />
+      <div className="main__container">
+        <h1>Material UI</h1>
+        <h2>Prueba de funcionamiento</h2>
+        <Button variant="contained">Clickear</Button>
+      </div>
     </div>
   );
 }
